39182f3dcb21a55caf37a1cc4827fd181d6ce3ec
mikelaslam <kipkorir385@gmail.com>
2022-12-12 22:58:28 +0300

    Buy tickets button

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     fetchMovies();
+    movieDetails();
 
 });
 //Initiating the fetch request to the server
 function fetchMovies() {
     fetch('http://localhost:3000/Movies')
-      .then(res => res.json())
+      .then(resp => resp.json())
       .then(data => {
         renderMovies(data);
         movieDetails(data[0]);
@@ -18,41 +19,41 @@ function fetchMovies() {
 function renderMovies(data) {
     const movieList =document.querySelector('ul#list');
     
-    for(let i = 0; < data.lenght; i++) {
-        const movie = data[i]
-        const filmLi = document.createElement('li');
-        filmLi.innerHTML = movie.tittle;
+    for(let i = 0; i < data.length; i++) {
+        const movie = data[i];
+        const filmli = document.createElement('li');
+        filmli.innerHTML = movie.title;
 
         //creating a line break to separate the movies
         const line = document.createElement('hr');
         movieList.appendChild(line);
-        movieList.appendChild(filmLi);
-        filmLi.addEventListener('click', function(){
+        movieList.appendChild(filmli);
+        filmli.addEventListener('click', function(){
             movieDetails(movie);
         }
-        )
-    }
+        
+    )}
 }
 //Rendering the movie details to the page
 function movieDetails(movie) {
     const movieDetails = document.getElementById('show-movie');
-    movieDetails.innerHTML = `
-    <h2>Tittle: ${movie.tittle}</h2>
+    movieDetails.innerHTML =`
+    <h2>Title: ${movie.title}</h2>
     <p> RunTime: ${movie.runtime} minutes</p>
     <p> Capacity: ${movie.capacity}</p>
     <p> Show Time: ${movie.showtime}</p>
-    <img scr= "${source.poste}">
+    <img src="${movie.poster}">
     <p> ${movie.description}</p>
-    <p> id = "tickets">Tickets Available: ${movie.tickets_sold}</p>
+    <p id = "tickets">Tickets Available: ${movie.tickets_sold}</p>
     <ul id ="users"><ul>
     <button id= "buy">Buy Tickets</button>
-
-    `
+    
+     `
     buyTicket(movie)
 }
-//Buying the ticket and displaying the remaining tickets
+// //Buying the ticket and displaying the remaining tickets
 function buyTicket(movie) {
-    const buyButton =document.getElementById('buy')
+    const buyButton = document.getElementById('buy')
     buyButton.addEventListener('click', function(e) {
         e.preventDefault()
         const tickets = document.getElementById('tickets')
